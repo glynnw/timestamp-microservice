89bbeffb2d1e20a04f8d4b59637b767df3435f19
Matthew Westbrook <horrorvacui@tutanota.com>
2016-11-22 20:04:36 -0600

    Add gitignore file and change to pug templates

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ var dateformat = require('dateformat');
 
 app.use(compression());
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 app.get('/', function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render('index', { host: request.get('Host') });
 });
 
 app.get("/:timestamp", function (request, response) {
   var time = new Date(request.params.timestamp);
   var result = { "unix": null, "natural": null };
-  if (time !== 'Invalid Date') {
+  if (time != 'Invalid Date') {
     result = {
       "unix": time.getTime(),
       "natural": dateformat(time, 'mmmm d, yyyy')
