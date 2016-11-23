305124db88a248d9704f836e24204bcc74f9a526
Matthew Westbrook <horrorvacui@tutanota.com>
2016-11-22 18:41:40 -0600

    Clean up style and useless comments

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 var compression = require('compression');
 var dateformat = require('dateformat');
 
 app.use(compression());
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 app.get('/', function (request, response) {
@@ -20,8 +11,8 @@ app.get('/', function (request, response) {
 });
 
 app.get("/:timestamp", function (request, response) {
-  let time = new Date(request.params.timestamp);
-  let result = { "unix": null, "natural": null };
+  var time = new Date(request.params.timestamp);
+  var result = { "unix": null, "natural": null };
   if (time != 'Invalid Date') {
     result = {
       "unix": time.getTime(),
@@ -31,7 +22,6 @@ app.get("/:timestamp", function (request, response) {
   response.jsonp(result);
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
